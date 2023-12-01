1a934f815bd474dedd1773f62a4c90d3edb2fdc2
mishaiem <152499263+mishaiem@users.noreply.github.com>
2023-12-01 19:08:29 +0500

    getstarted

---
+++ app/src/main/java/com/example/scrapping_app/Screen/GetStartedActivity.java
@@ -0,0 +1,28 @@
+package com.example.scrapping_app.Screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.scrapping_app.R;
+
+public class GetStartedActivity extends AppCompatActivity {
+    Button loginbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+        loginbtn=findViewById(R.id.loginbtn);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(GetStartedActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+}
