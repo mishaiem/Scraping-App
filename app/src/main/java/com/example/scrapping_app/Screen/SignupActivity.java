79d037e6cff437df6f9e86508ec664cfd35674f4
mishaiem <azhar45711@gmail.com>
2023-12-05 18:58:22 +0500

    signup

--- app/src/main/java/com/example/scrapping_app/Screen/SignupActivity.java
+++ app/src/main/java/com/example/scrapping_app/Screen/SignupActivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.scrapping_app.R;
+import com.example.scrapping_app.databinding.ActivitySignupBinding;
 
 public class SignupActivity extends AppCompatActivity {
 
+    ActivitySignupBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        binding=ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        binding.name,
+
+
 
     }
 }
